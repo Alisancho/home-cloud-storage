8418f46ba70ad6a1c3f85558badeb4450b7c819a
alisancho <sashabig4328@protonmail.com>
2020-06-17 23:28:13 +0300

    Add local logic

--- client/src/main/java/ru/client/AppStart.java
+++ client/src/main/java/ru/client/AppStart.java
@@ -5,17 +5,23 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
 
 public class AppStart extends Application {
 
     @Override
-    public void start(final Stage stage) {
-        final var javaVersion = System.getProperty("java.version");
-        final var javafxVersion = System.getProperty("javafx.version");
-        final var l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        final var scene = new Scene(new StackPane(l), 640, 480);
+    public void start(final Stage stage) throws IOException {
+        final Parent panel = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        final Scene scene = new Scene(panel, 1200, 700);
+        stage.setTitle("Client cloud");
+        scene.getStylesheets().add(getClass().getResource("/fontstyle.css").toExternalForm());
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+
     }
 
     public static void main(String[] args) {
