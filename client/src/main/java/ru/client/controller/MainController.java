7923dbe3e4cb17be0a6ff6aade8543549f8ccb50
alisancho <sashabig4328@protonmail.com>
2020-06-27 19:37:19 +0300

    fixController

--- client/src/main/java/ru/client/controller/MainController.java
+++ client/src/main/java/ru/client/controller/MainController.java
@@ -1,7 +1,11 @@
 package ru.client.controller;
 
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
 import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import io.vavr.control.Option;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,31 +15,39 @@ import javafx.scene.control.TreeTableColumn;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
-import ru.client.entity.OneFile;
+import ru.client.entity.OneFileFX;
 import ru.client.service.WorkWithFilesServiceImpl;
-import ru.home.entity.UserCloud;
+import ru.client.service.netty.NettyClientServiceImpl;
+import ru.home.api.entity.auth.UserCloud;
+import ru.home.api.entity.catalog.ContentsDirectory;
+import ru.home.api.entity.data.OneTask;
+import scala.concurrent.ExecutionContextExecutor;
 
+import java.io.IOException;
 import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
+
+import static ru.home.api.entity.TaskType.OPTIONS;
 
 public class MainController implements MainCiontrollerInt {
     private String pathLocal;
-    final private ObservableList<OneFile> filesListClient = FXCollections.observableArrayList();
-    final private TreeItem<OneFile> rootClient = new RecursiveTreeItem<OneFile>(filesListClient, RecursiveTreeObject::getChildren);
-    final private JFXTreeTableColumn<OneFile, String> typeColClient = new JFXTreeTableColumn<>(COL_1);
-    final private JFXTreeTableColumn<OneFile, String> nameColClient = new JFXTreeTableColumn<>(COL_2);
-    final private JFXTreeTableColumn<OneFile, String> sizeColClient = new JFXTreeTableColumn<>(COL_3);
+    private NettyClientServiceImpl nioClient;
 
-    final private ObservableList<OneFile> filesListServer = FXCollections.observableArrayList();
-    final private TreeItem<OneFile> rootServer = new RecursiveTreeItem<OneFile>(filesListClient, RecursiveTreeObject::getChildren);
-    final private JFXTreeTableColumn<OneFile, String> typeColServer = new JFXTreeTableColumn<>(COL_1);
-    final private JFXTreeTableColumn<OneFile, String> nameColServer = new JFXTreeTableColumn<>(COL_2);
-    final private JFXTreeTableColumn<OneFile, String> sizeColServer = new JFXTreeTableColumn<>(COL_3);
+    final static ActorSystem system = ActorSystem.create();
+    final static Materializer materializer = ActorMaterializer.create(system);
+    final static ExecutionContextExecutor exception = system.dispatcher();
 
-    @FXML
-    private ResourceBundle resources;
+    final private ObservableList<OneFileFX> filesListClient = FXCollections.observableArrayList();
+    final private TreeItem<OneFileFX> rootClient = new RecursiveTreeItem<OneFileFX>(filesListClient, RecursiveTreeObject::getChildren);
+    final private JFXTreeTableColumn<OneFileFX, String> typeColClient = new JFXTreeTableColumn<>(COL_1);
+    final private JFXTreeTableColumn<OneFileFX, String> nameColClient = new JFXTreeTableColumn<>(COL_2);
+    final private JFXTreeTableColumn<OneFileFX, String> sizeColClient = new JFXTreeTableColumn<>(COL_3);
 
-    @FXML
-    private VBox sbur;
+    final private ObservableList<OneFileFX> filesListServer = FXCollections.observableArrayList();
+    final private TreeItem<OneFileFX> rootServer = new RecursiveTreeItem<OneFileFX>(filesListServer, RecursiveTreeObject::getChildren);
+    final private JFXTreeTableColumn<OneFileFX, String> typeColServer = new JFXTreeTableColumn<>(COL_1);
+    final private JFXTreeTableColumn<OneFileFX, String> nameColServer = new JFXTreeTableColumn<>(COL_2);
+    final private JFXTreeTableColumn<OneFileFX, String> sizeColServer = new JFXTreeTableColumn<>(COL_3);
 
     @FXML
     private JFXButton connectButton;
@@ -44,28 +56,16 @@ public class MainController implements MainCiontrollerInt {
     private JFXButton disconnectButton;
 
     @FXML
-    private HBox mainBox;
-
-    @FXML
-    private JFXTreeTableView<OneFile> tableHome;
-
-    @FXML
-    private JFXTreeTableView<OneFile> tableServer;
-
-    @FXML
     private JFXButton upButton;
-
-    @FXML
-    private JFXTextField localAddressTextField;
 
     @FXML
     private JFXButton goToCatalog;
 
     @FXML
-    private JFXTextField loginTextField;
+    private JFXTextField localAddressTextField;
 
     @FXML
-    private JFXPasswordField passTextField;
+    private JFXTextField loginTextField;
 
     @FXML
     private JFXTextField hostTextField;
@@ -73,30 +73,74 @@ public class MainController implements MainCiontrollerInt {
     @FXML
     private JFXTextField portTextField;
 
+    @FXML
+    private JFXPasswordField passTextField;
+
+    @FXML
+    private JFXTreeTableView<OneFileFX> tableHome;
+
+    @FXML
+    private JFXTreeTableView<OneFileFX> tableServer;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private VBox sbur;
+
+    @FXML
+    private HBox mainBox;
 
     @FXML
     void initialize() throws InterruptedException {
         localAddressTextField.setText(System.getProperty("user.dir"));
+
         typeColClient.setPrefWidth(100);
-        typeColClient.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFile, String>, ObservableValue<String>>() {
+        nameColClient.setPrefWidth(400);
+        sizeColClient.setPrefWidth(150);
+
+        typeColServer.setPrefWidth(100);
+        nameColServer.setPrefWidth(400);
+        sizeColServer.setPrefWidth(150);
+
+        typeColClient.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFileFX, String>, ObservableValue<String>>() {
             @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFile, String> param) {
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFileFX, String> param) {
                 return param.getValue().getValue().fileType;
             }
         });
 
-        nameColClient.setPrefWidth(400);
-        nameColClient.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFile, String>, ObservableValue<String>>() {
+        nameColClient.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFileFX, String>, ObservableValue<String>>() {
             @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFile, String> param) {
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFileFX, String> param) {
                 return param.getValue().getValue().fileName;
             }
         });
 
-        sizeColClient.setPrefWidth(150);
-        sizeColClient.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFile, String>, ObservableValue<String>>() {
+        sizeColClient.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFileFX, String>, ObservableValue<String>>() {
             @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFile, String> param) {
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFileFX, String> param) {
+                return param.getValue().getValue().fileSize;
+            }
+        });
+
+        typeColServer.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFileFX, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFileFX, String> param) {
+                return param.getValue().getValue().fileType;
+            }
+        });
+
+        nameColServer.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFileFX, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFileFX, String> param) {
+                return param.getValue().getValue().fileName;
+            }
+        });
+
+        sizeColServer.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<OneFileFX, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<OneFileFX, String> param) {
                 return param.getValue().getValue().fileSize;
             }
         });
@@ -106,11 +150,50 @@ public class MainController implements MainCiontrollerInt {
         tableHome.setRoot(rootClient);
         tableHome.setShowRoot(false);
 
+        tableServer.getColumns().setAll(typeColServer, nameColServer, sizeColServer);
+        tableServer.prefHeight(60);
+        tableServer.setRoot(rootServer);
+        tableServer.setShowRoot(false);
+
+        /**
+         * Кнопка коннекта
+         */
         connectButton.setOnAction(event -> {
-            final var userCloud = new UserCloud(loginTextField.getText(), passTextField.getText());
-            mainBox.getChildren().remove(sbur);
-            disconnectButton.setVisible(true);
-            WorkWithFilesServiceImpl.getFiles(filesListClient, localAddressTextField.getText());
+            try {
+                nioClient = new NettyClientServiceImpl(hostTextField.getText(), Integer.parseInt(portTextField.getText()));
+//                final var userCloud = new UserCloud(loginTextField.getText(), passTextField.getText());
+                mainBox.getChildren().remove(sbur);
+                disconnectButton.setVisible(true);
+
+                WorkWithFilesServiceImpl.getFiles(filesListClient, localAddressTextField.getText());
+
+
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        while (true) {
+                            final var newMess = nioClient.readObject();
+                            if (newMess instanceof OneTask oneTask) {
+                                System.out.println("LPLPLPLPPL");
+                            }
+                            if (newMess instanceof ContentsDirectory contentsDirectory) {
+                                System.out.println("Получен список файлов" + contentsDirectory.toString());
+                                WorkWithFilesServiceImpl.getFiles(filesListServer, contentsDirectory.files());
+                            }
+                        }
+                    } catch (ClassNotFoundException | IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        nioClient.close();
+                    }
+                });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
         });
 
         disconnectButton.setOnAction(event -> {
@@ -120,11 +203,15 @@ public class MainController implements MainCiontrollerInt {
         });
 
         upButton.setOnAction(event -> {
+            this.nioClient.sendMsg(new OneTask(OPTIONS, Option.none(), Option.none()));
         });
 
         goToCatalog.setOnAction(event -> {
             WorkWithFilesServiceImpl.getFiles(filesListClient, localAddressTextField.getText());
         });
 
+        goToCatalog.setOnAction(event -> {
+            WorkWithFilesServiceImpl.getFiles(filesListClient, localAddressTextField.getText());
+        });
     }
 }
