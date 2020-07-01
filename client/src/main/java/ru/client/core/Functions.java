4dc4d716e757a9dabbe4e6c8764d257c72fd852c
alisancho <sashabig4328@protonmail.com>
2020-07-01 19:30:25 +0300

    Add akka logic

--- client/src/main/java/ru/client/core/Functions.java
+++ client/src/main/java/ru/client/core/Functions.java
@@ -6,7 +6,15 @@ import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import org.jetbrains.annotations.NotNull;
 import ru.client.entity.OneFileFX;
+import ru.home.api.entity.catalog.FileType;
+import ru.home.api.entity.catalog.OneServerFile;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 public class Functions {
 
@@ -22,7 +30,10 @@ public class Functions {
         );
         return Void.TYPE;
     };
-
+    /**
+     * Функция для заполнетни таблиц
+     * М для промежуточний данныз
+     */
     public static Function8<JFXButton, JFXButton, JFXButton, HBox, JFXButton, ObservableList<OneFileFX>, VBox, Throwable, Class<Void>> functionDisconnection = (putToServerButton, getFromServerButton, deleteFromServerButton, mainBox, disconnectButton, filesListServer, sbur, error) -> {
         Platform.runLater(
                 () -> {
@@ -36,5 +47,47 @@ public class Functions {
         );
         return Void.TYPE;
     };
+
+
+    /**
+     * Заполняем таблицу клиента
+     */
+    public static void getFiles(final @NotNull ObservableList<OneFileFX> obList,
+                                final @NotNull String filePath) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                obList.remove(0, obList.size());
+                final var files = new File(filePath).listFiles();
+                Platform.runLater(
+                        () -> {
+                            Arrays.stream(files).forEach(file ->
+                                    obList.add(new OneFileFX(FileType.getTypeFile(file), file.getName(), String.format("%.2f", (double) file.length() / 1024) + " kb"))
+                            );
+                        }
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Заполняем таблицу сервера
+     *
+     * @param obList
+     * @param fileSet
+     */
+    public static void getFiles(final @NotNull ObservableList<OneFileFX> obList,
+                                final @NotNull Set<OneServerFile> fileSet) {
+        CompletableFuture.runAsync(() -> {
+            Platform.runLater(
+                    () -> {
+                        obList.remove(0, obList.size());
+                        fileSet.forEach(l ->
+                                obList.add(new OneFileFX(l.fileType(), l.nameFile(), l.sizeFile())));
+                    }
+            );
+        });
+    }
 
 }
--- client/src/main/java/ru/client/service/netty/handler/ClientHandler.java
+++ client/src/main/java/ru/client/service/netty/handler/ClientHandler.java
@@ -1,5 +1,6 @@
 package ru.client.service.netty.handler;
 
+import akka.util.ByteString;
 import com.jfoenix.controls.JFXTextField;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -8,18 +9,17 @@ import io.vavr.control.Option;
 import javafx.collections.ObservableList;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import ru.client.core.Functions;
 import ru.client.entity.OneFileFX;
-import ru.client.service.WorkWithFilesServiceImpl;
 import ru.home.api.entity.ErrorType;
 import ru.home.api.entity.NettyMess;
-import ru.home.api.entity.TaskType;
 import ru.home.api.entity.catalog.ContentsDirectory;
 import ru.home.api.entity.data.OneTask;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
+import static java.nio.file.StandardOpenOption.*;
 import static ru.home.api.entity.ErrorType.ERROR_MESS;
 import static ru.home.api.entity.TaskType.OPTIONS;
 
@@ -50,6 +50,14 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
         }
     }
 
+    public void sendMessage(@NotNull final ByteString msgToSend) {
+        if (this.ctxMain != null) {
+            this.ctxMain.writeAndFlush(msgToSend);
+        } else {
+            log.info("ctx not initialized yet. you were too fast. do something here");
+        }
+    }
+
     @Override
     public void channelActive(ChannelHandlerContext ctxL) throws Exception {
         log.info("Client channelActive");
@@ -63,28 +71,38 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
         log.info("Новое сообщение на клиент");
         if (msg instanceof ContentsDirectory contentsDirectory) {
             log.info("Получен список файлов" + contentsDirectory.toString());
-            WorkWithFilesServiceImpl.getFiles(filesListServer, contentsDirectory.files());
+            Functions.getFiles(filesListServer, contentsDirectory.files());
         }
         if (msg instanceof OneTask oneTask) {
             log.info("Получен OneTask");
-            if (oneTask.task() == TaskType.GET) {
-                Files.write(Paths.get(localAddressTextField.getText() + "/" + oneTask.fileName().get()), oneTask.data().get(), StandardOpenOption.CREATE);
-                WorkWithFilesServiceImpl.getFiles(filesListClient, localAddressTextField.getText());
-            } else {
-                log.error("Ошибка в сообщении");
+            switch (oneTask.task()) {
+                case GET -> {
+                    Files.write(Paths.get(oneTask.path().get() + "/" + oneTask.fileName().get()),
+                            oneTask.byteString().get().toArray(),
+                            CREATE, WRITE, APPEND);
+                }
+                case OPTIONS -> {
+                    if (localAddressTextField.getText().equals(oneTask.path().get())) {
+                        Functions.getFiles(filesListClient, oneTask.path().get());
+                    }
+                }
+                default -> {
+                    log.error("Ошибка в сообщении");
+                }
             }
+
         }
         if (msg instanceof ErrorType errorType) {
             switch (errorType) {
                 case ERROR_NO -> {
-                    ctx.writeAndFlush(new OneTask(OPTIONS, Option.none(), Option.none()));
+                    ctx.writeAndFlush(new OneTask(OPTIONS, Option.none(), Option.none(), Option.none()));
                 }
                 case ERROR_MESS -> {
                     log.error(ERROR_MESS.errorType());
                 }
                 case AUTH_OK -> {
                     funOn.apply(null);
-                    ctx.writeAndFlush(new OneTask(OPTIONS, Option.none(), Option.none()));
+                    ctx.writeAndFlush(new OneTask(OPTIONS, Option.none(), Option.none(), Option.none()));
                 }
                 default -> {
                 }
